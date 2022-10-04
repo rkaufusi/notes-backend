1ca715348984754f5dee5a6df09a7b176da02708
rkaufusi <rhettkaufusi@gmail.com>
2022-10-04 16:13:24 -0700

    added basic error handling

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,20 @@
 const express = require("express");
-const { getNotes } = require("../database.js");
+const { getNotes, createNote, deleteNote } = require("../database.js");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   let notes = getNotes();
 	res.send(notes)
 });
+
+router.post("/create", (req, res) => {
+	const {noteTitle, note, UserID} = req.body;
+	createNote(ntoeTitle, note, userID)
+	res.send("note created");
+})
+
+router.delete("delete", (req, res) => {
+	
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,24 @@
 const express = require("express");
-const {login, createUser, getNotes} = require("../database.js");
+const {login, createUser, deleteUser} = require("../database.js");
 const router = express.Router();
 
 router.post("/login", async (req, res) => {
-	let {name, password} = req.body;
-	let isValidUser = await login(name, password);
-	console.log(isValidUser);
-  res.send(isValidUser);
+		let {name, password} = req.body;
+		let isValidUser = await login(name, password);
+		console.log(isValidUser);
+		res.send(isValidUser);
 });
 
 router.post("/create", (req, res) => {
 	let {name, password} = req.body;
 	createUser(name, password);
-  res.send("here");
+  res.send("user created");
 });
+
+router.delete("/delete", (req, res) => {
+	let { name } = req.body;
+	deleteUser(name);
+	res.send("user deleted");
+})
 
 module.exports = router;
