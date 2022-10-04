ae5ca7e87c8f6da79baaa939d7ceb89b4518141c
rkaufusi <rhettkaufusi@gmail.com>
2022-10-04 11:32:12 -0700

    added create tables if not exist

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql2");
 const bcrypt = require("bcryptjs");
-const user = process.env.USER;
+require("dotenv").config();
+const user = process.env.APP_USER;
 const password = process.env.DB_PASSWORD;
 
 const pool = mysql
@@ -12,21 +13,44 @@ const pool = mysql
   })
   .promise();
 
+async function createTables() {
+  await pool.query(`CREATE TABLE if not exists users (
+    UserID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255),
+    password varchar(255)
+	);`);
+
+  await pool.query(`CREATE TABLE if not exists notes (
+    NoteID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+		noteTitle varchar(255),
+    note varchar(255),
+    UserID int,
+    FOREIGN KEY (UserID) REFERENCES users(UserID)
+	);`);
+}
+
+createTables();
+
 async function login(userName, pass) {
   let [user] = await pool.query(`SELECT * FROM users WHERE username = ?`, [
     userName,
   ]);
-
-  async function createUser(userName, pass) {
-    await pool.query(`INSERT INTO users (username, password) VALUES (? , ?)`, [
-      userName,
-      pass,
-    ]);
-    return "user created";
-  }
-
-	async function getNotes(id){
-		let notes = pool.query(`SELECT * FROM notes WHERE id = ?`, [id]);
-		return notes;
-	}
 }
+async function createUser(userName, pass) {
+  await pool.query(`INSERT INTO users (username, password) VALUES (? , ?)`, [
+    userName,
+    pass,
+  ]);
+  return "user created";
+}
+
+async function getNotes(id) {
+  let notes = pool.query(`SELECT * FROM notes WHERE id = ?`, [id]);
+  return notes;
+}
+
+async function createNote(){
+	// stuff
+}
+
+module.exports = { pool, login, createUser, getNotes };
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/login", (req, res) => {
+  res.send("here");
+});
+
+router.get("/create", (req, res) => {
   res.send("here");
 });
 
